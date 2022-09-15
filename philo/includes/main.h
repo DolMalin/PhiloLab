d87297f59762143a9d4de608138a4f53e32148fd
DolMalin <dolmalinn@gmail.com>
2022-09-15 16:40:00 +0200

    feat(utils): add utils files with ft_strlen

--- philo/includes/main.h
+++ philo/includes/main.h
@@ -6,7 +6,7 @@
 /*   By: pdal-mol <dolmalinn@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 16:02:03 by pdal-mol          #+#    #+#             */
-/*   Updated: 2022/09/15 16:02:34 by pdal-mol         ###   ########.fr       */
+/*   Updated: 2022/09/15 16:35:15 by pdal-mol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,21 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <stdio.h>
+
+/* ================ ~ STRUCTURES ~ ================ */
+
+typedef enum	e_bool
+{
+	false,
+	true
+}				t_bool;
+
+/* ================ ~ UTILS ~ ================ */
+size_t	ft_strlen(const char *str);
+
+/* ================ ~ PARSING ~ ================ */
+t_bool	check_input(int ac, char **av);
+
 
 #endif
