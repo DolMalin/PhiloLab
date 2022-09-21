2a829bc9c9f7d47c023430fee783cadd7fe86f4d
Ana Andric <aandric@z3r3p5.42lyon.fr>
2022-09-21 09:37:58 +0200

    fix(github) come back to main branch needs update

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,27 +6,27 @@
 /*   By: aandric <aandric@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 16:03:44 by pdal-mol          #+#    #+#             */
-/*   Updated: 2022/09/16 14:12:36 by aandric          ###   ########lyon.fr   */
+/*   Updated: 2022/09/21 09:37:02 by aandric          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/main.h"
 
-t_data	*init_data(int ac, char **av)
+/* ================================================= */
+/*  DEBUG */
+void	log_forks(t_data *data)
 {
-	t_data	*data;
-
-	data = malloc(sizeof(t_data));
-	if (!data)
-		return (NULL);
-	data->philo_nb = ft_atoi(av[1]);
-	data->time_to_die = ft_atoi(av[2]);
-	data->time_to_eat = ft_atoi(av[3]);
-	data->time_to_sleep = ft_atoi(av[4]);
-	if (ac == 6)
-		data->meals_nb = ft_atoi(av[5]);
-	return (data);
+	printf("FORKS ID:\n");
+	for (int i = 0; i < data->philo_nb; i++)
+		printf("\tFORKS #%d: %p\n", i, &data->forks_array[i]);
+	for (int i = 0; i < data->philo_nb; i++)
+	{
+		printf("PHILO #%d\n", i);
+		printf("\tFORK L: %p\n", data->philo_array[i]->fork_l);
+		printf("\tFORK R: %p\n", data->philo_array[i]->fork_r);	
+	}	
 }
+/* ================================================= */
 
 int	main(int ac, char **av)
 {
@@ -34,11 +34,15 @@ int	main(int ac, char **av)
 
 	if (!check_input(ac, av))
 		return (0);
-	data = init_data(ac, av);
+	data = data_init(ac, av);
 	if (!data)
 		return (0);
-	data->philo_array = philo_create_array(data->philo_nb);
-	philo_free(data->philo_array, data->philo_nb);
-	free(data);
+	data->philo_array = philo_create_array(data);
+	if (!data->philo_array)
+	{
+		free(data);
+		return (0);
+	}
+	data_free(data);
 	return (0);
 }
